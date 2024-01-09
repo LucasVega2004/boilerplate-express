7e27df8f9affbf57d9433f798e5fc58c18915c54
LucasVega2004 <luquitasvegablanco@gmail.com>
2024-01-09 19:03:36 +0000

    part 2

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World")
+app.get('/', function (req, res) {
+    res.send('Hello Express');
+})
 
 
 
@@ -37,4 +39,5 @@ console.log("Hello World")
 
 
 
- module.exports = app;
+
+module.exports = app;
